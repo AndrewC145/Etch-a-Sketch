fc5a65bab67fbc1e4bfad8f06f4c8a449aed48b2
Andrew Cao <andrewcao618@gmail.com>
2024-11-02 22:04:32 -0400

    Reorganized Code Format to put startingGrid() in the beginning

--- script.js
+++ script.js
@@ -4,6 +4,33 @@ const newGrid = document.querySelector('.grid-num');
 
 const eraser = document.querySelector('.erase-btn');
 
+// Defines a Starting Size of the Grid
+function startingGrid(size) {
+  // Starting Values for the Grid
+  let gridAmount = size * size;
+  let gridSize = 600 / size; // 600px is the size of the container
+
+  // Creates the Grid and Loops over the amount entered
+  for (let i = 0; i < gridAmount; i++) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.style.width = `${gridSize}px`;
+    grid.style.height = `${gridSize}px`;
+    grid.style.border = '1px solid grey';
+
+    // Changes the Color of the Grid when Hovered Over
+    grid.addEventListener('mouseover', () => {
+      grid.style.backgroundColor = 'black';
+    });
+
+    // Appends the Grid to the Container
+    container.appendChild(grid);
+  }
+}
+
+startingGrid(16);
+
+
 newGrid.addEventListener('click', () => {
   // Prompts the User how big they want the Grid to be when Clicked
   const gridValue = parseInt(prompt('How big do you want the grid to be? Must be between 1 and 100'));
@@ -34,33 +61,6 @@ newGrid.addEventListener('click', () => {
     container.appendChild(grid);
   }
 });
-
-
-// Defines a Starting Size of the Grid
-function startingGrid(size) {
-  // Starting Values for the Grid
-  let gridAmount = size * size;
-  let gridSize = 600 / size; // 600px is the size of the container
-
-  // Creates the Grid and Loops over the amount entered
-  for (let i = 0; i < gridAmount; i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    grid.style.width = `${gridSize}px`;
-    grid.style.height = `${gridSize}px`;
-    grid.style.border = '1px solid grey';
-
-    // Changes the Color of the Grid when Hovered Over
-    grid.addEventListener('mouseover', () => {
-      grid.style.backgroundColor = 'black';
-    });
-
-    // Appends the Grid to the Container
-    container.appendChild(grid);
-  }
-}
-
-startingGrid(16);
 
 eraser.addEventListener('click', () => {
   const grids = document.querySelectorAll('.grid');
