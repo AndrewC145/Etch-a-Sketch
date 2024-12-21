37cb35b36d201ca04181cf1d327a3ce0fe40f3cb
Andrew Cao <andrewcao618@gmail.com>
2024-12-20 21:07:23 -0500

    Reformatted with Less Lines

--- script.js
+++ script.js
@@ -1,82 +1,54 @@
-const container = document.querySelector('.container');
-const newGrid = document.querySelector('.grid-num');
-const drawing = document.querySelector('.draw');
-const eraser = document.querySelector('.erase-btn');
+const newGrid = document.querySelector(".grid-num");
+const draw = document.querySelector(".draw");
+const erase = document.querySelector(".erase-btn");
+const container = document.querySelector(".container");
+const reset = document.querySelector(".reset-btn");
 
-// Defines a Starting Size of the Grid
-function startingGrid(size) {
-  // Starting Values for the Grid
+let gridAmount = 16;
+createGrid(gridAmount);
+
+function createGrid(size) {
+  let gridSize = 600 / size;
   let gridAmount = size * size;
-  let gridSize = 600 / size; // 600px is the size of the container
 
-  // Creates the Grid and Loops over the amount entered
   for (let i = 0; i < gridAmount; i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
+    const grid = document.createElement("div");
     grid.style.width = `${gridSize}px`;
     grid.style.height = `${gridSize}px`;
-    grid.style.border = '1px solid grey';
-
-    // Changes the Color of the Grid when Hovered Over
-    grid.addEventListener('mouseover', () => {
-      grid.style.backgroundColor = 'black';
-    });
-
-    // Appends the Grid to the Container
+    grid.classList.add("grid");
     container.appendChild(grid);
   }
 }
 
-// Sets starting grid size
-startingGrid(16);
 
-
-newGrid.addEventListener('click', () => {
-  // Prompts the User how big they want the Grid to be when Clicked
-  const gridValue = parseInt(prompt('How big do you want the grid to be? Must be between 1 and 100'));
-
-  // Checks if the Value is between 1 and 100, returns an alert if not
-  if (gridValue < 1 || gridValue > 100 || isNaN(gridValue)) {
-    alert('Please enter a number between 1 and 100');
+newGrid.addEventListener("click", () => {
+  gridAmount = parseInt(prompt("Enter a number between 1 and 100"));
+  if (gridAmount > 100 || gridAmount < 1 || isNaN(gridAmount)) {
+    alert("Please enter a number between 1 and 100");
     return;
   }
 
-  // Resets the grid when a new grid is created
-  container.textContent = '';
-  
-  let gridAmount = gridValue * gridValue;
-  let gridSize = 600 / gridValue;
-
-  // Creates the Grid and Loops over the amount entered
-  for (let i = 0; i < gridAmount; i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    grid.style.width = `${gridSize}px`;
-    grid.style.height = `${gridSize}px`;
-    grid.style.border = '1px solid grey';
-
-    grid.addEventListener('mouseover', () => {
-      grid.style.backgroundColor = 'black';
-    });
-    container.appendChild(grid);
-  }
+  container.textContent = "";
+  createGrid(gridAmount);
 });
 
-drawing.addEventListener('click', () => {
-  const grids = document.querySelectorAll('.grid');
-  grids.forEach(grid => {
-    grid.addEventListener('mouseover', () => {
-      grid.style.backgroundColor = 'black';
-    });
+draw.addEventListener("click", () => {
+  container.addEventListener("mouseover", event => {
+    if (event.target.classList.contains("grid")) {
+      event.target.style.backgroundColor = "black";
+    }
+  }); 
+});
+
+erase.addEventListener("click", () => {
+  container.addEventListener("mouseover", event => {
+    if (event.target.classList.contains("grid")) {
+      event.target.style.backgroundColor = "white";
+    }
   });
 });
 
-eraser.addEventListener('click', () => {
-  const grids = document.querySelectorAll('.grid');
-  // Loops through each grid item to add an event listener (grid is a variable initialized in the loop)
-  grids.forEach(grid => {
-    grid.addEventListener('mouseover', () => {
-      grid.style.backgroundColor = 'white';
-    });
-  });
+reset.addEventListener("click", () => {
+  container.textContent = "";
+  createGrid(gridAmount);
 });
